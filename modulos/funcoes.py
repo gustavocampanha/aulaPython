8f70876e319426ebb101159dc49c6d872d3dc51c
Gustavo Campanha <gustavo.gcf@outlook.com>
2022-10-10 09:15:18 -0300

    update

--- modulos/funcoes.py
+++ modulos/funcoes.py
@@ -82,16 +82,16 @@ def interface():
             list_inventario(dic)
 
         elif escolha == '2':
-            item = str(input('Digite o item que deseja adicionar')).lower()
-            quantidade = int('Quanto possui desse item?')
+            item = str(input('Digite o item que deseja adicionar: ')).lower()
+            quantidade = int('Quanto possui desse item? ')
             add_item(dic, item, quantidade)
             
         elif escolha == '3':
-            item = str(input('Digite o item que deseja excluir')).lower()
+            item = str(input('Digite o item que deseja excluir: ')).lower()
             rem_item(dic, item)
 
         elif escolha == '4':
-            item = str(input('Digite o item que deseja buscar')).lower()
+            item = str(input('Digite o item que deseja buscar: ')).lower()
             print(find_item(dic, item))
 
         elif escolha == '5':
