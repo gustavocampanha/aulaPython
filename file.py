7a888d172476ca16602f7f01f1f41fa3010b1f85
Gustavo Campanha <gustavo.gcf@outlook.com>
2022-10-10 08:26:16 -0300

    docstrings

---
+++ file.py
@@ -0,0 +1,32 @@
+def list_inventario(x):
+    """
+    Retorna o dicionário completo.
+    :param x: dict.
+    :return dict listando o inventário.
+    """
+    return True
+
+def add_item():
+    """
+    Adiciona item ao dicionário do inventário
+    :param x: dict.
+    :return dict adiciona item ao inventário
+    """
+    return True
+
+def rem_item(x):
+    """
+    Remove item ao dicionário do inventário
+    :param x: dict.
+    :return dict remove item ao inventário
+    """
+    return True
+
+def find_item(x, y):
+    """
+    Busca item no dicionário do inventário
+    :param x: dict.
+    :param y: str
+    :return Bool caso ache o item no inventário
+    """
+    return True
